d790467d0fe5d1e7070c3b0f3546eafa807d3f75
TiaWasTaken <arganetto.mattia@gmail.com>
2024-09-24 16:23:43 +0200

    added list components

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,42 @@
+import React, { useState } from "react";
 import "./chatList.css"
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
-    <div className ='chatList'>ChatList</div>
+    <div className ='chatList'>
+      <div className="search">
+        <div className="searchbar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Cerca" />
+        </div>
+        <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add" onClick={()=>setAddMode((prev)=>!prev)}/>
+      </div>
+      
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hey there! I am using your app.</p>
+        </div>
+      </div>
+
+      <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hey there! I am using your app.</p>
+          </div>
+      </div>
+      <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hey there! I am using your app.</p>
+          </div>
+      </div>
+
+    </div>
   )
 }
 
