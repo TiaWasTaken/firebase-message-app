f1e524024ffff1a871a37641cfd33443295303b1
TiaWasTaken <arganetto.mattia@gmail.com>
2024-09-25 15:58:41 +0200

    all sing up stuff done and linked to the storage, database and auth

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,9 @@ import "./login.css";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../lib/firebase";
+import { auth, db } from "../../lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import upload from "../../lib/upload";
 
 const Login = () => {
 
@@ -10,6 +12,8 @@ const Login = () => {
     file: null,
     url: ""
   })
+
+  const [loading, setLoading] = useState(false);
 
   const handleAvatar = (e) => {
     if(e.target.files[0]){
@@ -29,14 +33,30 @@ const Login = () => {
 
 const handleRegister = async (e) => {
   e.preventDefault();
+  setLoading(true);
   const formData = new FormData(e.target);
   
   const { username, email, password } = Object.fromEntries(formData);
   console.log(email, password);
 
   try {
-    const response = await createUserWithEmailAndPassword(auth, email, password);
-    console.log("User created:", response.user);
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+  
+    const imgUrl = await upload(avatar.file);
+
+    await setDoc(doc(db, "users", res.user.uid),{
+        username: username,
+        email: email,
+        avatar: imgUrl,
+        id: res.user.uid,
+        blocked: [],
+      });
+    
+    await setDoc(doc(db, "userchats", res.user.uid),{
+        chats: [],
+      });
+
+    console.log("User created:", res.user);
     document.body.style.overflow = "hidden"; 
     document.body.style.scrollbarWidth = "none";
     toast.success("User registered successfully");
@@ -47,6 +67,8 @@ const handleRegister = async (e) => {
     console.error("Error occurred during registration:", err);
 
     toast.error(err.message);
+  } finally {
+    setLoading(false);
   }
 };
 
@@ -57,7 +79,7 @@ const handleRegister = async (e) => {
         <form onSubmit={handleLogin}>
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign in</button>
+          <button disabled={loading}>{loading ? "Loading" : "Sign in"}</button>
         </form>
       </div>
       <div className="separator"></div>
@@ -71,7 +93,7 @@ const handleRegister = async (e) => {
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Sign up</button>
+          <button disabled={loading}>{loading ? "Loading" : "Sign up"}</button>
         </form>
       </div>
     </div>
