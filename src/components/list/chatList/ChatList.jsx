17e44f2620e491db03de08a04af6935bd5044537
TiaWasTaken <arganetto.mattia@gmail.com>
2024-09-25 11:25:38 +0200

    finished design

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./chatList.css"
+import AddUser from "./addUser/AddUser.jsx";
 
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false);
@@ -35,7 +36,7 @@ const ChatList = () => {
             <p>Hey there! I am using your app.</p>
           </div>
       </div>
-
+      {addMode && <AddUser />}
     </div>
   )
 }
